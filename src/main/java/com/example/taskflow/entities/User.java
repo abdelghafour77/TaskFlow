0bd6511404c00db1e41472888e00498f10e7108b
abdelghafour <miso.abdo@gmail.com>
2023-12-25 16:56:38 +0100

    remove spring security and config liquibase

--- src/main/java/com/example/taskflow/entities/User.java
+++ src/main/java/com/example/taskflow/entities/User.java
@@ -2,24 +2,19 @@ package com.example.taskflow.entities;
 
 import com.example.taskflow.entities.enums.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.*;
 
 import java.util.Collection;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Builder
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User  {
 
     @Id
     @GeneratedValue
@@ -31,33 +26,4 @@ public class User implements UserDetails {
     private Role role;
 
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
--- src/main/java/com/example/taskflow/repositories/UserRepository.java
+++ src/main/java/com/example/taskflow/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.taskflow.repositories;
 
 import com.example.taskflow.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
