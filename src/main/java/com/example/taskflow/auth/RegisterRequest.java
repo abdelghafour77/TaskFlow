a059eace33a33e4dfb373c0a3c6b2f907f23f6a8
abdelghafour <miso.abdo@gmail.com>
2023-12-30 20:18:32 +0100

    add validation

--- src/main/java/com/example/taskflow/auth/RegisterRequest.java
+++ src/main/java/com/example/taskflow/auth/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.example.taskflow.auth;
 
 import com.example.taskflow.entities.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+  @NotNull (message = "First name is required")
+  @NotBlank (message = "First name is required")
   private String firstname;
+  @NotNull (message = "Last name is required")
+  @NotBlank (message = "Last name is required")
   private String lastname;
+  @NotNull (message = "Email is required")
+  @NotBlank (message = "Email is required")
   private String email;
+  @NotNull (message = "Password is required")
+  @NotBlank (message = "Password is required")
   private String password;
+  @NotNull (message = "Role is required")
+  @NotBlank (message = "Role is required")
   private Role role;
 }
